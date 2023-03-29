90ea7ded095b6e16d78dbf77f0724f38a7be076b
Kangrui Ye <kangrui.ye@cloudflight.io>
2023-03-29 14:27:43 +0200

    implement support for opacity
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- src/filters/allowed-types.ts
+++ src/filters/allowed-types.ts
@@ -1,7 +1,7 @@
 import { TransformedToken } from 'style-dictionary';
 import { radiusType } from '../models/radius';
 
-const allowedTypes = ['dimension', 'string', 'number', 'color', radiusType];
+const allowedTypes = ['dimension', 'string', 'number', 'color', radiusType, 'custom-opacity'];
 
 export function filterForAllowedTypes(token: TransformedToken): boolean {
     return allowedTypes.includes(token['type']);
